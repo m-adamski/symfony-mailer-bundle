053ed37d9d2d50d7fafc8092424be524cbfaf7d4
Maciej Adamski <m.adamski@ideacode.pl>
2019-05-29 11:37:40 +0200

    Symfony 4.2 deprecation

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -11,13 +11,18 @@ class Configuration implements ConfigurationInterface {
      * {@inheritdoc}
      */
     public function getConfigTreeBuilder() {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root("mailer");
+        $treeBuilder = new TreeBuilder("mailer");
+
+        if (method_exists($treeBuilder, "getRootNode")) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            $rootNode = $treeBuilder->root("mailer");
+        }
 
         $rootNode
             ->children()
-                ->scalarNode("sender_address")->defaultValue("noreply@example.com")->end()
-                ->scalarNode("sender_name")->defaultValue("no-reply")->end()
+            ->scalarNode("sender_address")->defaultValue("noreply@example.com")->end()
+            ->scalarNode("sender_name")->defaultValue("no-reply")->end()
             ->end();
 
         return $treeBuilder;
